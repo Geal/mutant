4d041e78ea3c09f235fd2fc7a16256528eca82f4
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-13 00:19:00 +0100

    fix typo

--- tests/main.rs
+++ tests/main.rs
@@ -2,7 +2,7 @@
 #![mutant]
 #![plugin(mutant)] extern crate mutant;
 
-// the function returns 12, but mutation testing will make it return 12
+// the function returns 12, but mutation testing will make it return 42
 fn forty_two() -> u8 {
   return 12;
 }
